e74faa7f7b61667f90a211ba92a2e24e6fbed662
Atharva Sen <atharvasenbarai@gmail.com>
2022-01-15 22:03:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@
 
 // Firebase Config
 var firebaseConfig = {
-  apiKey: "**********",
-  authDomain: "**********",
-  databaseURL: "**********",
-  projectId: "**********",
-  storageBucket: "**********",
-  messagingSenderId: "**********",
-  appId: "**********",
-  measurementId: "**********"
+ apiKey: "AIzaSyDIdSpACQbc1-_JICLQ6gRjl_YrX25eiOA",
+  authDomain: "mini-diary-organization.firebaseapp.com",
+  databaseURL: "https://mini-diary-organization-default-rtdb.firebaseio.com",
+  projectId: "mini-diary-organization",
+  storageBucket: "mini-diary-organization.appspot.com",
+  messagingSenderId: "542075449644",
+  appId: "1:542075449644:web:26133a6448c78f79927c89",
+  measurementId: "G-26RQZMG8W6"
 };
 
 // Initialize Firebase
